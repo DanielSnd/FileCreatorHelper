31858014c6d2beb6c998c5183124914196c91300
DanielSnd <daniel.snd.anim@gmail.com>
2020-02-26 19:15:35 -0800

    Initial Commit

---
+++ FileCreatorHelper/Form1.Designer.cs
@@ -0,0 +1,252 @@
+﻿using System;
+using FastColoredTextBoxNS;
+
+namespace FileCreatorHelper
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.fastColoredTextBox1 = new FastColoredTextBoxNS.FastColoredTextBox();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.loadTemplateToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.loadFilesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exportToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.clearAllToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ExtensionName = new System.Windows.Forms.ToolStripMenuItem();
+            this.setExtensionToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.fastColoredTextBox1)).BeginInit();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // treeView1
+            // 
+            this.treeView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.treeView1.Location = new System.Drawing.Point(0, 0);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.Size = new System.Drawing.Size(245, 547);
+            this.treeView1.TabIndex = 4;
+            this.treeView1.NodeMouseDoubleClick += new System.Windows.Forms.TreeNodeMouseClickEventHandler(this.treeView1_AfterSelect);
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(3, 3);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.treeView1);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.fastColoredTextBox1);
+            this.splitContainer1.Size = new System.Drawing.Size(737, 547);
+            this.splitContainer1.SplitterDistance = 245;
+            this.splitContainer1.TabIndex = 5;
+            // 
+            // fastColoredTextBox1
+            // 
+            this.fastColoredTextBox1.AutoCompleteBracketsList = new char[] {
+        '(',
+        ')',
+        '{',
+        '}',
+        '[',
+        ']',
+        '\"',
+        '\"',
+        '\'',
+        '\''};
+            this.fastColoredTextBox1.AutoIndentCharsPatterns = "\r\n^\\s*[\\w\\.]+(\\s\\w+)?\\s*(?<range>=)\\s*(?<range>[^;]+);\r\n";
+            this.fastColoredTextBox1.AutoScrollMinSize = new System.Drawing.Size(27, 14);
+            this.fastColoredTextBox1.BackBrush = null;
+            this.fastColoredTextBox1.BracketsHighlightStrategy = FastColoredTextBoxNS.BracketsHighlightStrategy.Strategy2;
+            this.fastColoredTextBox1.CharHeight = 14;
+            this.fastColoredTextBox1.CharWidth = 8;
+            this.fastColoredTextBox1.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.fastColoredTextBox1.DisabledColor = System.Drawing.Color.FromArgb(((int)(((byte)(100)))), ((int)(((byte)(180)))), ((int)(((byte)(180)))), ((int)(((byte)(180)))));
+            this.fastColoredTextBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.fastColoredTextBox1.IsReplaceMode = false;
+            this.fastColoredTextBox1.Language = FastColoredTextBoxNS.Language.JS;
+            this.fastColoredTextBox1.LeftBracket = '(';
+            this.fastColoredTextBox1.LeftBracket2 = '{';
+            this.fastColoredTextBox1.Location = new System.Drawing.Point(0, 0);
+            this.fastColoredTextBox1.Name = "fastColoredTextBox1";
+            this.fastColoredTextBox1.Paddings = new System.Windows.Forms.Padding(0);
+            this.fastColoredTextBox1.RightBracket = ')';
+            this.fastColoredTextBox1.RightBracket2 = '}';
+            this.fastColoredTextBox1.SelectionColor = System.Drawing.Color.FromArgb(((int)(((byte)(60)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(255)))));
+            this.fastColoredTextBox1.ServiceColors = ((FastColoredTextBoxNS.ServiceColors)(resources.GetObject("fastColoredTextBox1.ServiceColors")));
+            this.fastColoredTextBox1.Size = new System.Drawing.Size(488, 547);
+            this.fastColoredTextBox1.TabIndex = 0;
+            this.fastColoredTextBox1.Zoom = 100;
+            this.fastColoredTextBox1.TextChangedDelayed += new System.EventHandler<FastColoredTextBoxNS.TextChangedEventArgs>(this.fastColoredTextBox1_TextChanged);
+            this.fastColoredTextBox1.Load += new System.EventHandler(this.fastColoredTextBox1_Load);
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Controls.Add(this.splitContainer1, 0, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 24);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 1;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(743, 553);
+            this.tableLayoutPanel1.TabIndex = 6;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.ExtensionName});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(743, 24);
+            this.menuStrip1.TabIndex = 7;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.loadTemplateToolStripMenuItem,
+            this.loadFilesToolStripMenuItem,
+            this.exportToolStripMenuItem,
+            this.clearAllToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // loadTemplateToolStripMenuItem
+            // 
+            this.loadTemplateToolStripMenuItem.Name = "loadTemplateToolStripMenuItem";
+            this.loadTemplateToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.loadTemplateToolStripMenuItem.Text = "Load Template";
+            this.loadTemplateToolStripMenuItem.Click += new System.EventHandler(this.loadTemplateToolStripMenuItem_Click);
+            // 
+            // loadFilesToolStripMenuItem
+            // 
+            this.loadFilesToolStripMenuItem.Name = "loadFilesToolStripMenuItem";
+            this.loadFilesToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.loadFilesToolStripMenuItem.Text = "Load Files";
+            this.loadFilesToolStripMenuItem.Click += new System.EventHandler(this.loadFilesToolStripMenuItem_Click);
+            // 
+            // exportToolStripMenuItem
+            // 
+            this.exportToolStripMenuItem.Name = "exportToolStripMenuItem";
+            this.exportToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.exportToolStripMenuItem.Text = "Export Files";
+            this.exportToolStripMenuItem.Click += new System.EventHandler(this.exportToolStripMenuItem_Click);
+            // 
+            // clearAllToolStripMenuItem
+            // 
+            this.clearAllToolStripMenuItem.Name = "clearAllToolStripMenuItem";
+            this.clearAllToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.clearAllToolStripMenuItem.Text = "Clear All";
+            this.clearAllToolStripMenuItem.Click += new System.EventHandler(this.clearAllToolStripMenuItem_Click);
+            // 
+            // ExtensionName
+            // 
+            this.ExtensionName.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.setExtensionToolStripMenuItem});
+            this.ExtensionName.Name = "ExtensionName";
+            this.ExtensionName.Size = new System.Drawing.Size(69, 20);
+            this.ExtensionName.Text = "Extension";
+            // 
+            // setExtensionToolStripMenuItem
+            // 
+            this.setExtensionToolStripMenuItem.Name = "setExtensionToolStripMenuItem";
+            this.setExtensionToolStripMenuItem.Size = new System.Drawing.Size(143, 22);
+            this.setExtensionToolStripMenuItem.Text = "Set Extension";
+            this.setExtensionToolStripMenuItem.Click += new System.EventHandler(this.setExtensionToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(743, 577);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "File Creator";
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.fastColoredTextBox1)).EndInit();
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        private void fastColoredTextBox1_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if(currentEditingNode != null)
+            {
+                currentEditingNode.Tag = fastColoredTextBox1.Text;
+            }
+        }
+
+        #endregion
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private FastColoredTextBoxNS.FastColoredTextBox fastColoredTextBox1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exportToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem ExtensionName;
+        private System.Windows.Forms.ToolStripMenuItem setExtensionToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem loadTemplateToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem loadFilesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem clearAllToolStripMenuItem;
+    }
+}
+
---
+++ FileCreatorHelper/Form1.cs
@@ -0,0 +1,251 @@
+﻿using Microsoft.WindowsAPICodePack.Dialogs;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Security;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FileCreatorHelper
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            // Create the ContextMenuStrip.
+            docMenu = new ContextMenuStrip();
+
+            //Create some menu items.
+            ToolStripMenuItem deleteLabel = new ToolStripMenuItem();
+            deleteLabel.Text = "Delete";
+
+            //Add the menu items to the menu.
+            docMenu.Items.AddRange(new ToolStripMenuItem[]{
+        deleteLabel});
+            deleteLabel.Click += DeleteNode;
+            // Set the ContextMenuStrip property to the ContextMenuStrip.
+            treeView1.ContextMenuStrip = docMenu;
+        }
+
+        private void DeleteNode(object sender, EventArgs e)
+        {
+            // use SourceControl property.. ContextMenuStrip must be associated with TreeView
+            ContextMenuStrip cms = (ContextMenuStrip)((ToolStripMenuItem)sender).Owner;
+            TreeView treeView = (TreeView)cms.SourceControl;
+            TreeNode node = treeView.GetNodeAt(treeView.PointToClient(cms.Location));
+            if (currentEditingNode == node) currentEditingNode = null;
+            if (templateNode == node) templateNode = null;
+            treeView.Nodes.Remove(node);
+        }
+
+        string templateString;
+        TreeNode templateNode = null;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+        }
+
+        private ContextMenuStrip docMenu;
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        TreeNode _currentNode;
+        TreeNode currentEditingNode
+        {
+            get
+            {
+                return _currentNode;
+            }
+            set
+            {
+                if(_currentNode != null)
+                {
+                    _currentNode.BackColor = Color.Empty;
+                }
+                _currentNode = value;
+                if(_currentNode != null)
+                {
+                    _currentNode.BackColor = Color.Lavender;
+                }
+            }
+        }
+
+        private void treeView1_AfterSelect(object sender, TreeNodeMouseClickEventArgs e)
+        {
+            currentEditingNode = e.Node;
+            fastColoredTextBox1.Text = (string)e.Node.Tag;
+            //MessageBox.Show((string)e.Node.Tag);
+        }
+
+        private void fastColoredTextBox1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        string newFileExtensions = "";
+        private void setExtensionToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            newFileExtensions = InputBox.ShowDialog("What extension would you like for the exported files?", "Set Extension", ".tile");
+            ExtensionName.Text = "Extension: " + newFileExtensions;
+        }
+
+        private void exportToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if(templateNode == null || templateNode.Nodes.Count <=0)
+            {
+                MessageBox.Show("No data found to export");
+                return;
+            }
+            CommonOpenFileDialog dialog = new CommonOpenFileDialog();
+            dialog.InitialDirectory = "C:\\Users";
+            dialog.IsFolderPicker = true;
+            if (dialog.ShowDialog() == CommonFileDialogResult.Ok)
+            {
+                string folderPath = dialog.FileName;
+                int amountToExport = templateNode.Nodes.Count;
+                string example = folderPath + @"\" + templateNode.Nodes[0].Text+newFileExtensions;
+                DialogResult dialogResult = MessageBox.Show(string.Format("This will export {0} files with the extension '{1}'.\nExample file: ' {2} ' \n Proceed?", amountToExport,newFileExtensions, example), "Exporting", MessageBoxButtons.YesNo);
+                if (dialogResult == DialogResult.Yes)
+                {
+                    bool fail = false;
+                    foreach (TreeNode item in templateNode.Nodes)
+                    {
+                        string filePath = folderPath + @"\" + item.Text + newFileExtensions;
+                        try
+                        {
+                            if (File.Exists(filePath)) File.Delete(filePath);
+                            File.WriteAllText(filePath, (string)item.Tag);
+                        } catch (UnauthorizedAccessException)
+                        {
+                            MessageBox.Show("You don't have enough authorization to save files in this folder. If you need to save it there then make sure you run the app as an administrator");
+                            fail = true;
+                            break;
+                        }
+                    }
+                    if(!fail)
+                    {
+                        DialogResult nDialogResult = MessageBox.Show("Files exported successfully. Would you like to open the export folder?", "Success", MessageBoxButtons.YesNo);
+                        if (nDialogResult == DialogResult.Yes)
+                        {
+                            System.Diagnostics.Process.Start(folderPath);
+                        }
+                        else if (nDialogResult == DialogResult.No)
+                        {
+                            //do something else
+                        }
+                    }
+                }
+                else if (dialogResult == DialogResult.No)
+                {
+                    //do something else
+                }
+            }
+        }
+
+        private void loadTemplateToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.Multiselect = false;
+            templateNode = null;
+            treeView1.Nodes.Clear();
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    foreach (var item in openFileDialog1.FileNames)
+                    {
+                        using (StreamReader sr = new StreamReader(item))
+                        {
+                            templateString = sr.ReadToEnd();
+                            TreeNode node = new TreeNode("Template");
+                            node.Text = "Template";
+                            if(!templateString.Contains("{0}"))
+                            {
+                                MessageBox.Show("{0} not found. Double click Template to edit the template and add {0} where you want the filenames of the files to appear");
+                            }
+                            node.ToolTipText = "Template";
+                            node.Tag = templateString;
+                            treeView1.Nodes.Add(node);
+                            templateNode = node;
+                            //string fileName = Path.GetFileNameWithoutExtension(item);
+                            //Template.Text = string.Format(line, fileName);
+                        }
+                    }
+                }
+                catch (SecurityException ex)
+                {
+                    MessageBox.Show($"Security error.\n\nError message: {ex.Message}\n\n" +
+                    $"Details:\n\n{ex.StackTrace}");
+                }
+            }
+        }
+
+        private void loadFilesToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if(templateNode == null)
+            {
+                MessageBox.Show("First you need to load a template file");
+                return;
+            }
+            openFileDialog1.Multiselect = true;
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    foreach (var item in openFileDialog1.FileNames)
+                    {
+                        string fileNameNoExtension = Path.GetFileNameWithoutExtension(item);
+                        TreeNode node = new TreeNode(fileNameNoExtension);
+                        if (templateString.Contains("{0}"))
+                            node.Tag = templateString.Replace("{0}", fileNameNoExtension);
+                        else node.Tag = templateString;
+                        node.Text = fileNameNoExtension;
+                        node.ToolTipText = fileNameNoExtension;
+                        if (templateNode != null)
+                        {
+                            templateNode.Nodes.Add(node);
+                        }
+                        else
+                        {
+                            treeView1.Nodes.Add(node);
+                        }
+                        treeView1.ExpandAll();
+                    }
+                }
+                catch (SecurityException ex)
+                {
+                    MessageBox.Show($"Security error.\n\nError message: {ex.Message}\n\n" +
+                    $"Details:\n\n{ex.StackTrace}");
+                }
+            }
+        }
+
+        private void clearAllToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            DialogResult dialogResult = MessageBox.Show("Are you sure you want to erase all the temporary data? You can't recover that data.", "Cler All", MessageBoxButtons.YesNo);
+            if (dialogResult == DialogResult.Yes)
+            {
+                _currentNode = null;
+                templateNode = null;
+                treeView1.Nodes.Clear();
+            }
+            else if (dialogResult == DialogResult.No)
+            {
+                //do something else
+            }
+        }
+    }
+}
